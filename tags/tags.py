363cbc86aceffa38c61722a3a3935161ae3de4c9
Bront <Brentholt_1@outlook.com>
2020-10-30 13:26:01 -0400

    Update tags.py

--- tags/tags.py
+++ tags/tags.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 from core import checks
 from core.models import PermissionLevel
+from .models import apply_vars, SafeString
 
 
 class TagsPlugin(commands.Cog):
@@ -11,6 +12,35 @@ class TagsPlugin(commands.Cog):
         self.bot: discord.Client = bot
         self.db = bot.plugin_db.get_partition(self)
 
+    def apply_vars_dict(self, member, message, invite):
+        for k, v in message.items():
+            if isinstance(v, dict):
+                message[k] = self.apply_vars_dict(member, v, invite)
+            elif isinstance(v, str):
+                message[k] = apply_vars(self, member, v, invite)
+            elif isinstance(v, list):
+                message[k] = [self.apply_vars_dict(member, _v, invite) for _v in v]
+            if k == 'timestamp':
+                message[k] = v[:-1]
+        return message
+
+    def format_message(self, member, message, invite):
+        try:
+            message = json.loads(message)
+        except json.JSONDecodeError:
+            # message is not embed
+            message = apply_vars(self, member, message, invite)
+            message = {'content': message}
+        else:
+            # message is embed
+            message = self.apply_vars_dict(member, message, invite)
+
+            if any(i in message for i in ('embed', 'content')):
+                message['embed'] = discord.Embed.from_dict(message['embed'])
+            else:
+                message = None
+        return message
+        
     @commands.group(invoke_without_command=True)
     @commands.guild_only()
     @checks.has_permissions(PermissionLevel.REGULAR)
