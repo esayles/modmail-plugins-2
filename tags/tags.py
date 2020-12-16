0706158f760f54dab7ae94881c9b011456b0cd77
Bront <Brentholt_1@outlook.com>
2020-12-16 13:49:08 -0500

    Update tags.py

--- tags/tags.py
+++ tags/tags.py
@@ -51,7 +51,7 @@ class TagsPlugin(commands.Cog):
             return
         
     @tags.command(name='list')
-    async def list_(self, ctx: commands.Context):
+    async def list_(self, ctx: commands.Context name:str):
         """
         Show list of commands
         """
