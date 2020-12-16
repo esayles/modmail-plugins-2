27e44daaf628bdf0396362737baf9f42fd2837a0
Bront <Brentholt_1@outlook.com>
2020-12-16 14:21:23 -0500

    Update tags.py

--- tags/tags.py
+++ tags/tags.py
@@ -55,7 +55,7 @@ class TagsPlugin(commands.Cog):
         """
         Show list of commands
         """ 
-        tag = await self.bot.db.getIndexes({"name":})
+        tag = await self.bot.db.getIndexes(name=name)
 
         if tag:
             await ctx.send('Tags: ' + ', '.join(tag))
